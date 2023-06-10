efe7cba2ba093a298f43585476c9252e13bf48e1
Viktoriya Ivleva <yagodkavi@mail.ru>
2023-06-10 22:13:44 +0300

    five seminar

--- 19.py
+++ 19.py
@@ -13,3 +13,22 @@ steps = int(input("Введите кол-во сдвигов"))
 steps = steps % len(data)
 data = [data[i-steps] for i in range(len(data))]
 print(data)
+
+'''
+n = int(input())
+list_first = list()
+list_result = list()
+for i in range(n):
+    list_first.append(int(input()))
+print(*list_first)
+
+k = int(input())
+k = k%n
+if k<0:
+    k=-k
+for i in range(k):
+    list_result[i]=list_first[n - k + i]
+for i in range(n - k):
+    list_result[k+i] = list_first[i]
+print(list_result)
+    '''
